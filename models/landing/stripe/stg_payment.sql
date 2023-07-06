3031bc50bb8de99576eda9e2031a3294181a00e3
svniekerk@oss-group.co.nz <svniekerk@oss-group.co.nz>
2023-07-06 04:42:55 +0000

    Add customers model, tests, docs.

---
+++ models/landing/stripe/stg_payment.sql
@@ -0,0 +1,13 @@
+with payments as (
+
+    select
+            id as payment_id,
+            orderid,
+            paymentmethod,
+            status,
+            amount,
+            created
+    from {{ source ('stripe','payment')}}
+)
+
+select * from payments
---
+++ models/raw/core/fact_orders.sql
@@ -0,0 +1,17 @@
+with orders as (
+    select * from {{ ref("stg_orders")}}
+),
+
+payments as (
+    select * from {{ ref("stg_payment")}}
+),
+
+fact_orders as(
+    select order_id,
+           payment_id,
+           amount
+    from orders
+    left join payments on orders.order_id = payments.orderid
+)
+
+select * from fact_orders
